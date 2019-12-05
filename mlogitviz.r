f5e5b02769cafb71abb0a94bd304691134f023b9
kelseygonzalez <kelseygonzalez@email.arizona.edu>
2019-12-05 14:32:17 -0700

    update function, add in auto printouts of tables and graphs

--- mlogitviz.r
+++ mlogitviz.r
@@ -1,98 +1,117 @@
 # CLustering predicted probabilities
 # Kelsey Gonzalez
-# 2019-08-27
+# 2019-05-27
+# last update: 2019-12-05
 
 rm(list = ls()) # clean up the environment
 require(foreign)
+require(mlogit)
+
+ml <- read.dta("https://stats.idre.ucla.edu/stat/data/hsbdemo.dta")
 
 
 ##############################################################################################################
 # main function
 ##############################################################################################################
 
-
-
-
 runmodel = function(my, variables, mdata){
   # loading packages (and install first)
-  packages = c("foreign", "nnet", "ggplot2", "reshape2")
+  packages = c("foreign", "nnet", "ggplot2", "reshape2", "gridExtra", "svglite","sjPlot", "sjmisc", "sjlabelled")
   for( i in packages ){
     if( ! require( i , character.only = TRUE ) ){
       install.packages(i, dependencies = TRUE )
-      require(i, character.only = TRUE )
+      require(i, character.only = TRUE)
     }
   }
   # run multinomial logit model
   model = multinom(as.formula(paste(my, "~", paste(variables, collapse ="+"))), data = mdata)
+  tab_model(model, p.style = "asterisk", string.resp = TRUE, show.ci = FALSE, file = "output.html")
+
   # create empty matrix to fill with results. set width by outcome variable length. 
-  predictions = matrix(nrow=0, ncol=length(levels(mdata[,get("my")])))
-  colnames(predictions) = levels(mdata[,get("my")])
+  pred = matrix(nrow=0, ncol=length(levels(mdata[,get("my")])))
+  colnames(pred) = levels(mdata[,get("my")])
+  
   # gather predicted probabilities for each variable level for factor variables
   for (var in variables){
     if (class(mdata[,get("var")]) == "factor") {
       for (one_level in levels(mdata[,get("var")])){
-        predictions = rbind(colMeans(fitted(model)[mdata[,get("var")] == one_level,]), predictions)
-        rownames(predictions)[1] = paste0(var,"; ",one_level)
+        pred = rbind(colMeans(fitted(model)[mdata[,get("var")] == one_level,]), pred)
+        rownames(pred)[1] = paste0(var,"; ",one_level)
       }
     }
+  
   # for numerical variables, run predicted probabilities with min, median and max. 
     if (class(mdata[,get("var")]) == "numeric"){
+      
+      #get descriptive statistics for the variable
+      five_num = fivenum(mdata[,get("var")])
+      
       # Min
-      predictions = rbind(colMeans(fitted(model)[mdata[,get("var")] == (fivenum(mdata[,get("var")])[1]),]), predictions)
-      rownames(predictions)[1] = paste0(var,"; Min")
+      fitted_1 = fitted(model)[mdata[,get("var")] == (five_num[1]),]
+      if (class(fitted_1) == "numeric") {fitted_1 = t(as.matrix(fitted_1))}
+      col_Means = colMeans(fitted_1)      
+      pred = rbind(col_Means, pred)
+      rownames(pred)[1] = paste0(var,"; Min")
       
       # Median
-      predictions = rbind(colMeans(fitted(model)[mdata[,get("var")] == (fivenum(mdata[,get("var")])[3]),]), predictions)
-      rownames(predictions)[1] = paste0(var,"; Median")
+      fitted_1 = fitted(model)[mdata[,get("var")] == (five_num[3]),]
+      if (class(fitted_1) == "numeric") {fitted_1 = t(as.matrix(fitted_1))}
+      col_Means = colMeans(fitted_1)      
+      pred = rbind(col_Means, pred)
+      rownames(pred)[1] = paste0(var,"; Median")
       
       # Max
-      predictions = rbind(colMeans(fitted(model)[mdata[,get("var")] == (fivenum(mdata[,get("var")])[5]),]), predictions)
-      rownames(predictions)[1] = paste0(var,"; Max")
+      fitted_1 = fitted(model)[mdata[,get("var")] == (five_num[5]),]
+      if (class(fitted_1) == "numeric") {fitted_1 = t(as.matrix(fitted_1))}
+      col_Means = colMeans(fitted_1)      
+      pred = rbind(col_Means, pred)
+      rownames(pred)[1] = paste0(var,"; Max")
       
       if (length(unique(mdata[,get("var")])) > 5) {
         # 1st Quart
-        predictions = rbind(colMeans(fitted(model)[mdata[,get("var")] == (fivenum(mdata[,get("var")])[2]),]), predictions)
-        rownames(predictions)[1] = paste0(var,"; 1stquart")
+        fitted_1 = fitted(model)[mdata[,get("var")] == (five_num[2]),]
+        if (class(fitted_1) == "numeric") {fitted_1 = t(as.matrix(fitted_1))}
+        col_Means = colMeans(fitted_1)      
+        pred = rbind(col_Means, pred)
+        rownames(pred)[1] = paste0(var,"; 1stquart")
         
         # 3rd Quart
-        predictions = rbind(colMeans(fitted(model)[mdata[,get("var")] == (fivenum(mdata[,get("var")])[4]),]), predictions)
-        rownames(predictions)[1] = paste0(var,"; 3rdquart")
+        fitted_1 = fitted(model)[mdata[,get("var")] == (five_num[4]),]
+        if (class(fitted_1) == "numeric") {fitted_1 = t(as.matrix(fitted_1))}
+        col_Means = colMeans(fitted_1)      
+        pred = rbind(col_Means, pred)
+        rownames(pred)[1] = paste0(var,"; 3rdquart")
       }
-      
     }
   }
-  predictions
+  # until I fixed the fact that fivenum may give values no representative in the dataset, 
+  # I will remove variables that are NA
+  pred <- na.omit(pred) 
+  write.csv(pred, file = "1_unsorted_pred.csv")
   
-  # until I fixed the fact that fivenum may give values no representative in the dataset, I will remove variables that are NA
-  predictions <- na.omit(predictions) 
   
-  #define euclidean distance function
-  euc.distance <- function(x,y) {
-    dist <- sum((x - y)^2)
-    dist <- sqrt(dist)
-    return(dist)
-  }
+  # perform hierarchical clustering on a euclidean distance matrix
+  hc <- hclust(d = dist(pred, method = "euclidean"), method = "ward.D2")
+  plot(hc, cex = 0.6, hang = -1, main="Hierarchal Cluster of Predicted Probabilities")
+  abline(h=quantile(dist(pred, method = "euclidean"), .75))
+  rect.hclust(hc, h=quantile(dist(pred, method = "euclidean"), .75), border = 2:5)
+
+    # Use that line (75% quartile of distance measures) to create cutoff point for number of groups using the average distance
+  mycl <- cutree(hc, h=quantile(dist(pred, method = "euclidean"), .75))
+  mycl
+  print(mycl)
+
+  # Let's sort our scaled matrix by hc ordering
+  matrix.sort <- (pred)[hc$order,]
+  write.csv(matrix.sort, file = "2_sorted_pred.csv")
   
-  #create distance matrix (AE) with scaled matrix by column
-  AE <- matrix(0, nrow = nrow(scale(predictions)), ncol = nrow(scale(predictions)))
-  rownames(AE) <- colnames(AE) <- rownames(scale(predictions))
-  for (i in 1:(nrow(scale(predictions))-1)) {
-    for (j in (i + 1):nrow(scale(predictions))) {
-      AE[j,i] <- AE[i,j] <- euc.distance(scale(predictions)[i,], scale(predictions)[j,])
-    }}
+  scaledmatrix.sort <- (scale(matrix.sort))
+  write.csv(scaledmatrix.sort, file = "3_scaled_sorted_pred.csv")
   
-  #perform hierarchical clustering on distance matrix (AE)
-  hc <- hclust(d = as.dist(AE), method = "complete")
-  
-  #Use that line (75% quartile of distance measures) to create cutoff point for number of groups using the average distance
-  mycl <- cutree(hc, h=quantile(AE, .75))
-  
-  #Let's sort our scaled matrix by hc ordering
-  scaledmatrix.sort <- (scale(predictions))[hc$order,]
-  predictions.long <- melt(predictions[hc$order,], id = rownames(predictions))
-  colnames(predictions.long)[colnames(predictions.long)=="value"] <- "originalvalue"
-  
-  # Let's develop a sorted list of cluster memberships. The cluster memberships are from  mycl <- cutree(hc, h=quantile(AE, .75)) of foo:
+  pred.long <- melt(pred[hc$order,], id = rownames(pred))
+  colnames(pred.long)[colnames(pred.long)=="value"] <- "originalvalue"
+
+  # Let's develop a sorted list of cluster memberships. The cluster memberships are from  mycl <- cutree(hc, h=quantile(distance_matrix, .75)) of foo:
   clusmemb <-  mycl[hc$order]
   
   # PRESERVING THIS ORDER, let's find the number of members of each cluster:
@@ -126,37 +145,34 @@ runmodel = function(my, variables, mdata){
   colnames(scaledmatrix.sort.long)[colnames(scaledmatrix.sort.long)=="value"] <- "Column\nZ-Score"
   
   # allows original values to label the cells instead of scaled values
-  scaledmatrix.sort.long <- cbind(scaledmatrix.sort.long, predictions.long)
+  scaledmatrix.sort.long <- cbind(scaledmatrix.sort.long, pred.long)
 
   #create heatmap with rows as outcomns, columns as categorical variables, and colors as high to low from scaling
-  heatmap.plot <- ggplot(data = scaledmatrix.sort.long, aes(x = Outcomes, y = Predictors)) +
-    scale_fill_gradient2(low = "red4", mid = "white",
-                         high = "forestgreen", midpoint = 0, space = "Lab",
-                         na.value = "grey50", guide = "colourbar", aesthetics = "fill") +
+  heatmap.plot <- ggplot(data = scaledmatrix.sort.long, 
+                         aes(x = Outcomes, 
+                             y = Predictors)) +
+    scale_fill_gradient2(low = "#fac877", 
+                         mid = "white",
+                         high = "#799250", 
+                         midpoint = 0, 
+                         space = "Lab",
+                         na.value = "grey50", 
+                         guide = "colourbar", 
+                         aesthetics = "fill") +
     geom_tile(aes(fill = `Column\nZ-Score`)) +  
-    scale_x_discrete(position = "top") +
+    scale_x_discrete(position = "top", expand = c(0, 0)) +
     ggtitle("Heatmap of Predicted Probabilities") +
     theme(legend.position = "right", axis.text.x = element_text(angle = 90)) +
     geom_segment(data=line, aes(x=X, y=Y, xend=Xend, yend=Yend)) +
-    geom_text(size = 2, aes(label = format(round(originalvalue, 3), nsmall = 3))) 
+    geom_text(size = 4, aes(label = format(paste0(round(originalvalue, 2)*100,"%"))))
+  
+  ggsave(file="finished.svg", plot=heatmap.plot, width=6.56, height=7.49)
 
-  print(heatmap.plot)
-  #perhaps figure out how to remove extra grey area in ggplot (padding?)
 }
 
-
-
-#load data variables for ease now
-ml <- read.dta("https://stats.idre.ucla.edu/stat/data/hsbdemo.dta")
-
-# works
-runmodel("prog", c("ses","write","female","schtyp","awards"), ml)
-
-# broken, why? need to debug
-runmodel("prog", c("ses","write","math","female","schtyp","awards"), ml)
 runmodel("prog", c("ses","write","math", "science", "female","schtyp","awards"), ml)
-# Error to debug: Error in colMeans(fitted(model)[mdata[, get("var")] == (fivenum(mdata[,  : 'x' must be an array of at least two dimensions 
-# Not sure where this error is originating, as soon as I add math etc it crashes the function
+runmodel("prog", c("ses","write","female","schtyp","awards"), ml)
+runmodel("prog", c("ses","write","math","female","schtyp","awards"), ml)
 
 
 # Let's test it with some real data...
@@ -181,6 +197,14 @@ Pew2013$generation <- as.factor(Pew2013$generation)
 summary(Pew2013)
 Pew2013 <- as.data.frame(Pew2013)
 
-runmodel("fivecat", c("RACE","AGE","PPARTY","EDUCCAT2","INCOMECAT"), Pew2013)
+runmodel("fivecat", c("RACE","AGE","ORIGIN","generation", "PPARTY","EDUCCAT2","INCOMECAT"), Pew2013)
 
 
+indig <- read.csv("Tribal Citizenship Database_import data analysis_FINAL.csv", stringsAsFactors = FALSE)
+indig$bia.region <- as.factor(indig$bia.region)
+indig$gaming <- as.factor(indig$gaming)
+indig$criteria <- as.factor(indig$criteria)
+indig$size <- as.numeric(indig$size)
+
+runmodel("criteria", c("gaming", "bia.region", "size"), indig)
+class(indig$size)
